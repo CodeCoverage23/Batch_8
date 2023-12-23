b584b5ff4397477f35b6badfe273d8e185984ce1
dell 5480 <dell 5480@DESKTOP-TDUQJND>
2023-12-23 17:21:01 +0530

    demo

---
+++ JavaProgram/src/com/java/program/operatorsjava.java
@@ -0,0 +1,35 @@
+package com.java.program;
+
+public class operatorsjava {
+
+	public static void main(String[] args) {
+		int a =10;//local variable
+		System.out.println(a);
+		
+		System.out.println(++a);
+		
+	    System.out.println(a++);
+	    
+	    System.out.println(a);
+	    
+	    System.out.println(--a);
+	    
+	    System.out.println(a--);
+	    
+	    System.out.println(a);
+	    
+	    
+	    
+		
+	
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
