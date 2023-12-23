64e1244347635950d3fc5122be4005d77c5e08c3
dhana <dhana@DHANU>
2023-12-23 08:58:34 +0530

    IfElse

---
+++ Java/src/JavaPackage/IfElse.java
@@ -0,0 +1,23 @@
+package JavaPackage;
+import java.util.Scanner;
+
+
+public class IfElse {
+    
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+	    System.out.println("Enter the number:");
+	    int num=sc.nextInt();
+	  
+		if(num%2==0)
+		{
+			System.out.println("It is even number");
+		}
+		else
+		{
+			System.out.println("It is odd number");
+		}
+
+	}
+
+}
