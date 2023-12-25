ac0ef880531b97d4b28b7fa75b87f501d834add8
dell 5480 <dell 5480@DESKTOP-TDUQJND>
2023-12-25 12:57:14 +0530

    integer

---
+++ JavaProgram/src/com/java/program/IntegerProgram.java
@@ -0,0 +1,18 @@
+package com.java.program;
+
+public class IntegerProgram {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=20;
+		
+		System.out.println(a=a+b);
+	
+		
+
+
+
+	}
+
+}
