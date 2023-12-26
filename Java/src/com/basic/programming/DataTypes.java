335cf1bdf2771d827d9a73e430add37c5b923c08
dhana <dhana@DHANU.mshome.net>
2023-12-26 22:04:03 +0530

    Assignment 2.1

---
+++ Java/src/com/basic/programming/DataTypes.java
@@ -0,0 +1,26 @@
+package com.basic.programming;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		int a = 01;
+		long l = 848303l;
+		short s = 2;
+		byte b = 50;
+		float f = 90.01f;
+		double d = 84830.39001d;
+		char c = 'D';
+		boolean bo = true;
+
+		System.out.println("Int Value:" + a);
+		System.out.println("Long Value:" + l);
+		System.out.println("Short Value:" + s);
+		System.out.println("Byte Value:" + b);
+		System.out.println("Float Value:" + f);
+		System.out.println("Double Value:" + d);
+		System.out.println("Char Value:" + c);
+		System.out.println("Boolean Value:" + bo);
+
+	}
+
+}
