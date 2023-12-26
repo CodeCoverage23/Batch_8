3b9831444002194910d16cc6efb5be99fcea4451
pratiksha <pratikshanaik3008@gmail.com>
2023-12-26 19:56:21 +0530

    Assignment 1

---
+++ Batch8/src/com/Assign1/Que1.java
@@ -0,0 +1,11 @@
+package com.Assign1;
+
+public class Que1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("the code coverage");
+
+	}
+
+}
