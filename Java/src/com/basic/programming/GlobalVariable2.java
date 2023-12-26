c90a2817796ffadf57da48052f34bd1f77003cad
dhana <dhana@DHANU.mshome.net>
2023-12-26 22:21:54 +0530

    Assignment 2.6

---
+++ Java/src/com/basic/programming/GlobalVariable2.java
@@ -0,0 +1,12 @@
+package com.basic.programming;
+
+public class GlobalVariable2 {
+	public static int variable;
+
+	public static void main(String[] args) {
+		variable = 25;
+		System.out.println(variable);
+
+	}
+
+}
