c89292ef6488f4f8c67e5d51447f57fce1b1cf77
LENOVO <LENOVO@LAPTOP-093DM0CO>
2023-12-27 17:15:07 +0530

    Assignment No.1

---
+++ Batch8/src/com/Assignment/MyClass.java
@@ -0,0 +1,15 @@
+package com.Assignment;
+
+//Write the java program to print the Code Coverage message on console.
+//Assignment no.1
+
+public class MyClass {
+	
+      public static void main(String[] args) {
+    	  
+    	  
+    	  System.out.println("Code Coverage");
+		
+	}
+
+}
