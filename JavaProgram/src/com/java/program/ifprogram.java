876e479a9a924f318c1a1f49dafc5918bc99a736
dell 5480 <dell 5480@DESKTOP-TDUQJND>
2023-12-27 21:09:20 +0530

    java

---
+++ JavaProgram/src/com/java/program/ifprogram.java
@@ -0,0 +1,14 @@
+package com.java.program;
+
+public class ifprogram {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		if (a==10) {
+			System.out.println("A is greater");
+		}
+
+	}
+
+}
