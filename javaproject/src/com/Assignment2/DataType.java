98f52f786855830875249cfa54a2608149844385
hp <hp@LAPTOP-SABT4769>
2023-12-27 19:07:05 +0530

    DataType

---
+++ javaproject/src/com/Assignment2/DataType.java
@@ -0,0 +1,20 @@
+package com.Assignment2;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		
+          byte bt;
+          short s;
+          int i;
+          long l;
+         double d;
+         float f;
+         boolean b;
+         char c;
+		
+		
+		
+	}
+
+}
