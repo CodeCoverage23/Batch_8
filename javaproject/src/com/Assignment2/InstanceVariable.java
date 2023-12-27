97a3caada29bbbbb0bdec00132f7e5e9289cb723
hp <hp@LAPTOP-SABT4769>
2023-12-27 19:27:14 +0530

    LocalVariable

---
+++ javaproject/src/com/Assignment2/InstanceVariable.java
@@ -0,0 +1,12 @@
+package com.Assignment2;
+
+public class InstanceVariable {
+   int b = 25;
+	public static void main(String[] args) {
+		
+		InstanceVariable InstanceVariable = new InstanceVariable();
+		System.out.println("Value of global variable is="+InstanceVariable .b);
+		
+	}
+
+}
