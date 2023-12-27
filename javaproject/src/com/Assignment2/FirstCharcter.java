f009c2756caef7cb84b53612e441e9563b6a5ca9
hp <hp@LAPTOP-SABT4769>
2023-12-27 19:10:33 +0530

    FirstCharacter

---
+++ javaproject/src/com/Assignment2/FirstCharcter.java
@@ -0,0 +1,11 @@
+package com.Assignment2;
+
+public class FirstCharcter {
+
+	public static void main(String[] args) {
+		
+ char ch = 'S';
+ System.out.println("first letter of my name is "+ch);
+	}
+
+}
