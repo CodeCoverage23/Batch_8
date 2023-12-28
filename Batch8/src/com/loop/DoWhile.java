d9fed886771700c1e6ef56a1eb337889ee3941a1
pratiksha <pratikshanaik3008@gmail.com>
2023-12-28 21:07:00 +0530

    do while loop

---
+++ Batch8/src/com/loop/DoWhile.java
@@ -0,0 +1,20 @@
+package com.loop;
+
+import java.util.Scanner;
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		int i = 0, j = 0;
+		Scanner sc = new Scanner(System.in);
+		do {
+
+			j += i;
+			System.out.println("j=" + j);
+			System.out.println("enter value of i : ");
+			i = sc.nextInt();
+		} while (i >= 0);
+
+	}
+
+}
