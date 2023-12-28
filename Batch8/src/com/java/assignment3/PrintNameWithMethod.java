b32a6b89209b2bbf73381e7620083a7b0e5b1c01
HP <HP@DESKTOP-8MV2VQ4>
2023-12-28 09:41:33 +0530

    Print Name With Default method

---
+++ Batch8/src/com/java/assignment3/PrintNameWithMethod.java
@@ -0,0 +1,16 @@
+package com.java.assignment3;
+
+//Design the separate method as String getStudentName (String name) which return student name and print it.
+public class PrintNameWithMethod {
+
+	public static void main(String[] args) {
+		System.out.println(PrintNameWithMethod.getStudentName());
+
+	}
+
+	public static String getStudentName() {
+		String name = "sujata";
+		return name;
+	}
+
+}
