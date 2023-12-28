8045687d9f06ba42cb5d54306e4ad312b1f41d01
Pragati <Pragati@LAPTOP-75OS1RCT>
2023-12-28 21:09:42 +0530

    method

---
+++ JavaProgram/src/com/Assignment/PrintNameWithMethod.java
@@ -0,0 +1,13 @@
+package com.Assignment;
+
+public class PrintNameWithMethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println(PrintNameWithMethod.getStudentName());
+		}
+    public static String getStudentName() {
+    String name="Pragati";
+    return name;
+ }
+}
