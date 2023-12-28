c2f1811e05c4ee32d8382010c509ffc24048e6bc
Pragati <Pragati@LAPTOP-75OS1RCT>
2023-12-28 19:45:38 +0530

    PrintName

---
+++ JavaProgram/src/com/Assignment/PrintNameAss1.java
@@ -0,0 +1,11 @@
+package com.Assignment;
+
+public class PrintNameAss1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+    System.out.println("Code Coverage");
+	}
+
+}
