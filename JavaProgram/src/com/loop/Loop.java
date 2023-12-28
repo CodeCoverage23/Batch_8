f33a7a5602ae73b30ba601931574b29d2bb96855
Pragati <Pragati@LAPTOP-75OS1RCT>
2023-12-28 20:25:53 +0530

    loop

---
+++ JavaProgram/src/com/loop/Loop.java
@@ -0,0 +1,14 @@
+package com.loop;
+
+public class Loop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   int i=0;
+   for(i=0;i<5;i++) {
+   }
+    System.out.println(i);
+   
+}
+}
+	
