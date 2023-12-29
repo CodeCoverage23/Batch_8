5d8a334d6572cc47c1aec446182b7f2f358306a2
dell 5480 <dell 5480@DESKTOP-TDUQJND>
2023-12-29 09:45:57 +0530

    java

---
+++ JavaProgram/src/com/java/codecoverage/Program1.java
@@ -0,0 +1,16 @@
+package com.java.codecoverage;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+		
+		for(int i=0;i<10;i++) {
+			if(i%2==0) {
+				continue; }
+				System.out.println(i); }
+			}
+		}
+
+	
+
+
---
+++ JavaProgram/src/com/java/codecoverage/Program2.java
@@ -0,0 +1,17 @@
+package com.java.codecoverage;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		
+		int i=0;
+		for(i=0;i<5;i++) {
+			
+		}
+		
+		//** Print statement is out of the loop.
+		
+		System.out.println(i);
+	
+	}
+	}
