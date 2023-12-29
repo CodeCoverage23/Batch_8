31dc17ba99a7b5522f2fcb265c1290bac52724d7
dell 5480 <dell 5480@DESKTOP-TDUQJND>
2023-12-29 16:11:10 +0530

    java

---
+++ JavaProgram/src/com/java/program/DataTypes.java
@@ -0,0 +1,28 @@
+package com.java.program;
+
+public class DataTypes {
+
+	byte b = 1;
+	
+	short s = 2;
+	
+	int a = 10;
+	
+	long l = 20;
+	
+	float f = 11.11f;
+	
+	double d = 12.12d;
+	
+	char c = 'A';
+	
+	boolean b1 = true;
+	
+	static int data;
+
+	public static void main(String[] args) {
+
+		System.out.println(data);
+	}
+
+}
