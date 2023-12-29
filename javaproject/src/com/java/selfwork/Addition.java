2bc1c565672ced5ef7631c78870780d6cdb361cd
hp <hp@LAPTOP-SABT4769>
2023-12-29 17:06:10 +0530

    Addition

---
+++ javaproject/src/com/java/selfwork/Addition.java
@@ -0,0 +1,27 @@
+package com.java.selfwork;
+
+import java.util.Scanner;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a,b,c;
+		
+		System.out.println(" enter any two number");
+		
+		Scanner sc= new Scanner (System.in);
+		
+		a= sc.nextInt();
+		b=sc. nextInt();
+		c=a+b;
+		
+		System.out.println("print sum = " +c);
+		
+		
+		
+		
+	}
+
+}
