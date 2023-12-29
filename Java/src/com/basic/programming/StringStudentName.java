b884e389ff66b1e9fdfcafc98e776f82b08e3b3e
dhana <dhana@DHANU>
2023-12-29 09:50:02 +0530

    Assignment 3

---
+++ Java/src/com/basic/programming/StringStudentName.java
@@ -0,0 +1,14 @@
+package com.basic.programming;
+
+public class StringStudentName {
+	public static void main(String[] args) {
+		System.out.println(StringStudentName.getStudentName());
+	}
+
+	public static String getStudentName()
+
+	{
+		String name = "Dhananjay";
+		return name;
+	}
+}
