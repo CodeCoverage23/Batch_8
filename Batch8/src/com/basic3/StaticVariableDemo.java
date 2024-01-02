9e57585c6757542d5e0ebf607a1b8d2d1eb29985
swapn <swapn@LAPTOP-BJNVAM67>
2024-01-02 21:10:41 +0530

    Static variable

---
+++ Batch8/src/com/basic3/StaticVariableDemo.java
@@ -0,0 +1,19 @@
+package com.basic3;
+
+public class StaticVariableDemo {
+
+	public static void main(String[] args) {
+
+		Student s1 = new Student(101, "Kishor");
+		Student s2 = new Student(102, "Pavan");
+		Student s3 = new Student(103, "Vishal");
+		Student s4 = new Student(104, "Pratik");
+		Student s5 = new Student(105, "Shiv");
+		Student s6 = new Student(106, "Shubham");
+
+		s1.display();
+		s2.display();
+		
+	}
+
+}
--- Batch8/src/com/basic3/Student.java
+++ Batch8/src/com/basic3/Student.java
@@ -4,15 +4,26 @@ public class Student {
 
 	int rollNo;
 	String name;
+	static String clgName = "ABC";
 
 	public Student(int rollNo, String name) {
 		this.rollNo = rollNo;
 		this.name = name;
 	}
 
+	public Student(int rollNo, String name, String clgName) {
+		this.rollNo = rollNo;
+		this.name = name;
+		this.clgName = clgName;
+
+		System.out.println(rollNo + " " + name + " " + clgName);
+	}
+
 	public void display() {
 		System.out.println("Roll Number : " + rollNo);
 		System.out.println("Name : " + name);
+		System.out.println("Clg Name : " + clgName);
+
 	}
 
 	/*
