ee1d818d5d160a5687b29edf7b1889775c36e78c
hp <hp@LAPTOP-SABT4769>
2024-01-08 16:13:53 +0530

    Assignment4Question3

---
+++ javaproject/src/com/Assignment4/Multiplication.java
@@ -0,0 +1,24 @@
+package com.Assignment4;
+
+import java.util.Scanner;
+
+public class Multiplication {
+
+	public static void main(String[] args) {
+		
+		System.out.println( " Multiplication");
+      System.out.println( Multiplication . mul() );
+	}
+	
+	public static int mul () {
+		
+		int a= 10;
+		int b= 15;
+		int c= a*b;
+		
+		return c;
+	}
+	
+	
+
+}
