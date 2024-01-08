9a375cffddc4f5ccd2e4ccc6a143ce5ba2bdb4cb
prade <prade@DESKTOP-9LC5689.lan>
2024-01-08 08:50:55 +0530

    assignment 4

---
+++ javaprogramming/src/com/java/programming/assignment4/Addition.java
@@ -0,0 +1,20 @@
+package com.java.programming.assignment4;
+//Write the java program to design method for addition of two number 
+//which returns int results to that method and result should be print into main method.
+
+
+public class Addition {
+
+	public static void main(String[] args) {
+		System.out.println(Addition.add());
+
+	}
+
+	public static int add() {
+		int a=10; int b=20;
+		int c=a+b;
+		return c;	
+	}
+	
+	
+}
---
+++ javaprogramming/src/com/java/programming/assignment4/Substraction.java
@@ -0,0 +1,19 @@
+package com.java.programming.assignment4;
+//Write the java program to design method for substraction of two number which returns int value to that method
+//and result should be print into main method.
+public class Substraction {
+
+	public static void main(String[] args) {
+		Substraction obj= new Substraction();
+		System.out.println(obj.sub(20, 10));
+
+	}
+
+	public int sub(int a , int b) {
+		int c=a-b;
+		return c;
+	}
+	
+	
+	
+}
