8025227996080a4548a8f7a0cb923840dbfe1833
hp <hp@LAPTOP-SABT4769>
2024-01-08 16:13:32 +0530

    Assignment4Question2

---
+++ javaproject/src/com/Assignment4/Substraction.java
@@ -0,0 +1,20 @@
+package com.Assignment4;
+
+public class Substraction {
+
+	public static void main(String[] args) {
+		
+	
+		Substraction obj= new Substraction();
+		System.out.println(obj.sub(10,20));
+	}
+	
+	
+	public int sub(int a ,int b) {
+		
+		int c=a-b;
+		return c;
+		
+	}
+
+}
