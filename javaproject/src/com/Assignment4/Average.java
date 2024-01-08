62aefd820e5614929e88ad26d90af6c0c0010198
hp <hp@LAPTOP-SABT4769>
2024-01-08 16:14:29 +0530

    Assignment4Question5

---
+++ javaproject/src/com/Assignment4/Average.java
@@ -0,0 +1,27 @@
+package com.Assignment4;
+
+public class Average {
+
+	public static void main(String[] args) {
+		
+		System.out.print(" Average of Five : ");
+		
+		System.out.println( Average.Average());
+	
+	}
+
+	public   static int Average () {
+		
+		int a=10;
+		int b=50;
+		int c=40;
+		int d=60;
+		int e=100;
+		int Avg = a+b+c+d+e/5;
+		
+		return c;
+		
+	}
+	
+	
+}
