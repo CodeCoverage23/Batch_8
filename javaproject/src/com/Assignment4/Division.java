59edc11710cb63ae3d6777f6e55fdb9b41281953
hp <hp@LAPTOP-SABT4769>
2024-01-08 16:14:14 +0530

    Assignment4Question4

---
+++ javaproject/src/com/Assignment4/Division.java
@@ -0,0 +1,21 @@
+package com.Assignment4;
+
+public class Division {
+
+	public static void main(String[] args) {
+		
+		
+		Division obj= new Division();
+		
+		System.out.println( obj. Division( 30,5));
+	}
+	
+	
+	 public int Division(int a, int b) {
+		 
+		 int c=a/b;
+		return c;
+		 
+	 }
+
+}
