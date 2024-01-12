1e9da56e0004b4a5873dc122e687efbc0531f413
dell 5480 <dell 5480@DESKTOP-TDUQJND>
2024-01-12 22:36:57 +0530

    oops

---
+++ JavaProgram/src/com/java/oops/Inheritance2.java
@@ -0,0 +1,37 @@
+package com.java.oops;
+
+/**
+* Implementation of Multilevel inheritance
+*/
+
+public class Inheritance2 {
+
+	public static void main(String[] args) {
+
+		BabyDog d = new BabyDog();
+
+		d.legs = 4;
+		d.walk();
+		d.bark();
+		d.eat();
+		System.out.println(d.legs);
+	}
+}
+
+class Dog extends Animal {
+
+	public void bark() {
+
+		System.out.println("Barking...");
+
+	}
+
+}
+
+class BabyDog extends Dog {
+
+	public void eat() {
+		System.out.println("Eating...");
+	}
+
+}
