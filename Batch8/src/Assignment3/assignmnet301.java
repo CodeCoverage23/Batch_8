a0fc64aca9b12b266acbb5277a575503fc92d77f
Schaurase <139573310+Schaurase@users.noreply.github.com>
2024-01-14 15:12:51 +0530

    assignment3

---
+++ Batch8/src/Assignment3/assignmnet301.java
@@ -0,0 +1,14 @@
+package Assignment3;
+//1. Design the separate method as String getStudentName (String name)
+//which return student name and print it.
+public class assignmnet301 {
+	public String getStudentName(String name) {
+		return name;
+	}
+	
+     public static void main (String[] arge) {
+    	 assignmnet301 obj = new assignmnet301();
+    	 String c = obj.getStudentName("shreyash");
+    	 System.out.println(c);
+     }
+}
