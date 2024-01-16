ec0cd1c0556c21dd7b1a90572be06a453e0971ce
LENOVO <LENOVO@LAPTOP-093DM0CO>
2024-01-16 23:35:56 +0530

    Assignment 3

--- Batch8/src/com/Assignment3/Example.java
+++ Batch8/src/com/Assignment3/Example.java
@@ -1,5 +1,25 @@
 package com.Assignment3;
 
+
+//Design the separate method as String getStudentName (String name) which return student name and print it.
+
+
 public class Example {
 
+	
+	String getStudentName (String name) {
+		return name;
+		
+		}
+	
+	public static void main(String[] args) {
+		
+		
+		Example example = new Example ();
+		String studentName = example.getStudentName("Nilima");
+		
+		System.out.println(studentName);
+	}
 }
+
+    
