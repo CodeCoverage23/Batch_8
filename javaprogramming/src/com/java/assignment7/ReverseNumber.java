a5b64130559c3d8b90f5193cf998f14347ce664c
prade <prade@DESKTOP-9LC5689.lan>
2024-01-18 09:03:24 +0530

    assignment 7

---
+++ javaprogramming/src/com/java/assignment7/ReverseNumber.java
@@ -0,0 +1,37 @@
+package com.java.assignment7;
+
+import java.util.Scanner;
+
+//4. Design method public int getReverseNumber(int num) which return int value to that method and
+//result print into main method.
+public class ReverseNumber {
+
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		System.out.println("Enter a number :");
+		int num = sc.nextInt();
+		int reverseNumber = ReverseNumber.getReverseNumber(num);
+       System.out.println("the reverse number of "+num+ " is :"+reverseNumber);
+
+	}
+
+	public static int getReverseNumber(int num) {
+		int reverse=0;
+		
+		while(num!=0) {
+		int lastDigit=num%10;
+		reverse=reverse*10;
+		reverse=reverse+lastDigit;
+			
+			num=num/10;
+			
+		}
+		
+		
+		
+		
+		return reverse;
+	}
+	
+	
+}
