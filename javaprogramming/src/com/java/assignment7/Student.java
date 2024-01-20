654c5f60d6a2fb7667e62d4db45e2c201124d51e
prade <prade@DESKTOP-9LC5689.lan>
2024-01-20 09:19:30 +0530

    assignment 8

---
+++ javaprogramming/src/com/java/assignment7/Student.java
@@ -0,0 +1,17 @@
+package com.java.assignment7;
+//Design the Student class which contain
+//firstName, lastName, city and mobileNumber and  country.
+
+public class Student {
+
+	String firstname;
+
+	String lastName;
+
+	String city;
+
+	long mobileNumber;
+
+	static String country = "india";
+
+}
---
+++ javaprogramming/src/com/java/assignment7/StudentInformation.java
@@ -0,0 +1,63 @@
+package com.java.assignment7;
+
+import java.util.Scanner;
+
+// Design the class as Student Information which takes the input for 
+//multiple students from user and Design method public void getUserInput(), 
+//country is same for all the students.
+
+public class StudentInformation {
+	public static void main(String[] args) {
+
+		StudentInformation info = new StudentInformation();
+		System.out.println("enter the number of Student :");
+		Scanner sc = new Scanner(System.in);
+		int number = sc.nextInt();
+
+		for (int i = 1; i <= number; i++) {
+			System.out.println("enter the Student Details>>");
+			info.getUserInput();
+
+		}
+
+	}
+
+	public void getUserInput() {
+
+		Student student = new Student();
+
+//		takes the input for multiple students from user
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the First name>>");
+		student.firstname = sc.nextLine();
+
+		System.out.println("Enter the last name>>");
+		student.lastName = sc.nextLine();
+
+		System.out.println("Enter the city>>");
+		student.city = sc.nextLine();
+
+		System.out.println("Enter the mobile Number>>");
+		student.mobileNumber = sc.nextLong();
+
+		getStudentInformation(student.firstname, student.lastName, student.city, student.mobileNumber, Student.country);
+
+	}
+
+	// Pass the input to getStudentInformation() method-parameterized method
+//	1.4 Print the multiple student information into getStudentInformation () method.
+
+	public void getStudentInformation(String fName, String lastName, String city, Long mobileNumber, String country) {
+
+		// Printing Student
+
+		System.out.println("student Information : ");
+		System.out.println("firstName : " + fName);
+		System.out.println("lastName : " + lastName);
+		System.out.println("city : " + city);
+		System.out.println("mobileNumbe : " + mobileNumber);
+		System.out.println("country : " + country);
+
+	}
+}
