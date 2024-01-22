54e9092fda89b981c69af1aab4cbd73ab3a2ac18
kalysure <kalysure@LIN-5CG3251NJY.corp.capgemini.com>
2024-01-22 14:48:54 +0530

    Average

---
+++ javaProgram/src/com/java/assignment4/Average.java
@@ -0,0 +1,20 @@
+package com.java.assignment4;
+
+public class Average {
+	
+
+	    public static int calculateAverage(int num1, int num2) {
+	        int average = (num1 + num2) / 2;
+	        return average;
+	    }
+
+	    public static void main(String[] args) {
+	        
+         int result = calculateAverage(5, 7);
+
+	       
+	        System.out.println("The average of two numbers is: " + result);
+	    }
+	}
+
+
