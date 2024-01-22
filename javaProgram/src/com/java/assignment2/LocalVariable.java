5d59a4a4c0cc60fb61735ca89bbeb19e736701db
kalysure <kalysure@LIN-5CG3251NJY.corp.capgemini.com>
2024-01-22 14:28:32 +0530

    LocalVariable

---
+++ javaProgram/src/com/java/assignment2/LocalVariable.java
@@ -0,0 +1,21 @@
+package com.java.assignment2;
+
+public class LocalVariable {
+
+	public class LocalVariableExample {
+	    public static void main(String[] args) {
+	      
+	        exampleMethod();
+	    }
+
+	    
+	    private static void exampleMethod() {
+	        
+	        int x = 10;
+
+	       
+	        System.out.println("The value of x: " + x);
+	    }
+	}
+
+}
