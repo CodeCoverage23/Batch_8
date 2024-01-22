563094684a41426ea69717cf9ce05143854e9d47
kalysure <kalysure@LIN-5CG3251NJY.corp.capgemini.com>
2024-01-22 14:54:15 +0530

    Multiplication

---
+++ javaProgram/src/com/java/assignment4/Multiplication.java
@@ -0,0 +1,22 @@
+package com.java.assignment4;
+
+public class Multiplication {
+
+
+	    
+	    public static int multiply(int num1, int num2) {
+	        int result = num1 * num2;
+	        return result;
+	    }
+
+	    public static void main(String[] args) {
+	       
+	        int result = multiply(5, 7);
+
+	        
+	        System.out.println("The result of multiplication is: " + result);
+	    }
+	}
+
+
+
