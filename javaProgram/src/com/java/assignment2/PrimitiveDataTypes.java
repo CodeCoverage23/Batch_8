672c6cf593bfe8b8dd6287d13481197616941ff8
kalysure <kalysure@LIN-5CG3251NJY.corp.capgemini.com>
2024-01-22 14:36:00 +0530

    PrimitiveDataTypes

---
+++ javaProgram/src/com/java/assignment2/PrimitiveDataTypes.java
@@ -0,0 +1,34 @@
+package com.java.assignment2;
+
+public class PrimitiveDataTypes {
+
+	    public static void main(String[] args) {
+	        
+	        byte byteVar = 10;
+	        short shortVar = 1000;
+	        int intVar = 50000;
+	        long longVar = 123456789L;
+
+	        float floatVar = 3.14f;
+	        double doubleVar = 2.71828;
+
+	        char charVar = 'A';
+
+	        boolean booleanVar = true;
+
+	        // Printing the values of the declared variables
+	        System.out.println("byteVar: " + byteVar);
+	        System.out.println("shortVar: " + shortVar);
+	        System.out.println("intVar: " + intVar);
+	        System.out.println("longVar: " + longVar);
+
+	        System.out.println("floatVar: " + floatVar);
+	        System.out.println("doubleVar: " + doubleVar);
+
+	        System.out.println("charVar: " + charVar);
+
+	        System.out.println("booleanVar: " + booleanVar);
+	    }
+	}
+
+
