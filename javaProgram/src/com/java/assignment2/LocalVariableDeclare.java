2a4012abf90abb7b6d97304758f9895d61a8de6c
kalysure <kalysure@LIN-5CG3251NJY.corp.capgemini.com>
2024-01-22 14:32:53 +0530

    LocalVariableDeclare

---
+++ javaProgram/src/com/java/assignment2/LocalVariableDeclare.java
@@ -0,0 +1,20 @@
+package com.java.assignment2;
+
+public class LocalVariableDeclare {
+	
+	    public static void main(String[] args) {
+	        
+	        assignValueToX();
+	    }
+
+	    
+	    private static void assignValueToX() {
+	        
+	        int x = 50;
+
+	       
+	        System.out.println("The value of x: " + x);
+	    }
+	}
+
+
