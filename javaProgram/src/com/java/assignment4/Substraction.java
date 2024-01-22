0020353a2bb2efaba7d0e564bc7630f5a592b48d
kalysure <kalysure@LIN-5CG3251NJY.corp.capgemini.com>
2024-01-22 14:57:29 +0530

    Substraction

---
+++ javaProgram/src/com/java/assignment4/Substraction.java
@@ -0,0 +1,19 @@
+package com.java.assignment4;
+
+public class Substraction {
+	
+
+	    public static int subtract(int a, int b) {
+	        int result = a - b;
+	        return result;
+	    }
+
+	    public static void main(String[] args) {
+	      
+	        int result = subtract(10, 3);
+
+	       
+	        System.out.println("The result of subtraction is: " + result);
+	    }
+	}
+
