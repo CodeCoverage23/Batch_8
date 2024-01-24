6ea8477e138ce2b428344a3b06e49d7cb318bddc
Pragati <Pragati@LAPTOP-75OS1RCT>
2024-01-24 17:01:05 +0530

    DataTypes

---
+++ JavaProgram/src/com/java/assignment2/DataTypes.java
@@ -0,0 +1,18 @@
+package com.java.assignment2;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		int a;
+		long l;
+		short s;
+		float f;
+		double d;
+		byte b;
+		char ch;
+		boolean e;
+		
+
+	}
+
+}
