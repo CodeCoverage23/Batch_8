d282d1f0a076ac96bc1e3b70057af7cd0809cbce
Pragati <Pragati@LAPTOP-75OS1RCT>
2024-01-24 17:04:11 +0530

    EvenNo

---
+++ JavaProgram/src/com/java/assignment6/EvenNo.java
@@ -0,0 +1,14 @@
+package com.java.assignment6;
+
+public class EvenNo {
+
+	public static void main(String[] args) {
+		
+    for(int i=10;i<=25;i++) {
+    	if(i%2==0) {
+    		System.out.println(i);
+    	}
+	}
+
+}
+}
