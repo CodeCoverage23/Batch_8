5b969c42cb733059162f3d80f4354b30d407c7dc
Pragati <Pragati@LAPTOP-75OS1RCT>
2024-01-24 16:50:56 +0530

    AvgProgram

---
+++ JavaProgram/src/com/Assignment4/Average.java
@@ -0,0 +1,20 @@
+package com.Assignment4;
+
+public class Average {
+
+	public static void main(String[] args) {
+		System.out.print("Average of five :");
+		System.out.println( Average.average());
+	}
+  public static int average() {
+	  int a=10; 
+	  int b=30; 
+	  int c=20;
+	  int d=40;
+	  int e=50;
+	  int Avg= a+b+c+d+e;
+	  int Avg1=Avg/5;
+	  
+	  return Avg1;
+  }
+}
