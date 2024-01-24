720fa02c8d2c0a07a60ac45d67db3c27cd7154fe
Pragati <Pragati@LAPTOP-75OS1RCT>
2024-01-24 17:09:04 +0530

    Multiplication

---
+++ JavaProgram/src/com/Assignment4/Multiplication.java
@@ -0,0 +1,16 @@
+package com.Assignment4;
+//WAP to design
+
+public class Multiplication {
+
+	public static void main(String[] args) {
+	
+       System.out.println(Multiplication.mul());
+	}
+	public static int mul() {
+		int a=10; int b=20;
+		int c=a*b;
+		return c;
+	}
+
+}
