9f6950f092b88ca7aa83f4355f3c77c886a6afdb
Pragati <Pragati@LAPTOP-75OS1RCT>
2024-01-24 17:07:49 +0530

    Substraction

---
+++ JavaProgram/src/com/Assignment4/Substraction.java
@@ -0,0 +1,18 @@
+package com.Assignment4;
+//WAP to design mtd for sub of two no
+// which returns int result to that mtd and result print into main mtd
+
+public class Substraction {
+
+	public static void main(String[] args) {
+		System.out.println(Substraction.sub());
+	}
+	public static int sub() {
+		int a=10;int b=20;
+		int c=a-b;
+		return c;
+		
+
+	}
+
+}
