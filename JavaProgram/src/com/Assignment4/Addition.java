24db71293677a0a47f16598548b35ec6a30cefd6
Pragati <Pragati@LAPTOP-75OS1RCT>
2024-01-24 17:00:24 +0530

    Addition

---
+++ JavaProgram/src/com/Assignment4/Addition.java
@@ -0,0 +1,16 @@
+package com.Assignment4;
+//WAP to design method for add of two no 
+//which returns int results to that mtd and result print into main mtd.
+
+public class Addition {
+
+	public static void main(String[] args) {
+		System.out.println(Addition.add());
+
+	}
+public static int add() {
+	int a=10; int b=20;
+	int c=a+b;
+	return c;
+}
+}
