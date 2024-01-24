9adb7ff139e61d92ee7bf2919eb8e5f2722068f6
Pragati <Pragati@LAPTOP-75OS1RCT>
2024-01-24 17:03:39 +0530

    Division

---
+++ JavaProgram/src/com/Assignment4/Division.java
@@ -0,0 +1,14 @@
+package com.Assignment4;
+
+public class Division {
+
+	public static void main(String[] args) {
+		System.out.println(Division.div());
+
+	}
+   public static float div() {
+	int a=26; int b=4;
+	 int c= a/b;
+	return c ;
+}
+}
