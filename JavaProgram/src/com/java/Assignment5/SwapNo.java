3b3c15f2a7a7d40e2dce0f8f5980f46e28d9d0d9
Pragati <Pragati@LAPTOP-75OS1RCT>
2024-01-24 16:59:24 +0530

    SwapNo

---
+++ JavaProgram/src/com/java/Assignment5/SwapNo.java
@@ -0,0 +1,22 @@
+package com.java.Assignment5;
+
+public class SwapNo {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int temp;
+		System.out.println("No before Swapping");;
+	System.out.println("a :"+a+" b:" +b);
+		
+		 temp = a;
+		a=b;
+		b=temp;
+		
+		System.out.println("No after swapping:");
+		
+		System.out.println("a :"+a+" b:" +b);
+
+	}
+
+}
