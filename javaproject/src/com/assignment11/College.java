b96aff784f3da0af7418a32f4e362667408f62d8
hp <hp@LAPTOP-SABT4769>
2024-01-25 09:11:28 +0530

    assignment11

---
+++ javaproject/src/com/assignment11/College.java
@@ -0,0 +1,10 @@
+package com.assignment11;
+
+public class College {
+
+	public static void main(String[] args) {
+		
+		Student s =new Student ();
+	}
+
+}
---
+++ javaproject/src/com/assignment11/Student.java
@@ -0,0 +1,13 @@
+package com.assignment11;
+/**
+ * Assignment 11
+ */
+public class Student {
+	
+
+	private Student() {
+		
+		System.out.println(" Private Constuctor ");
+	}
+	
+}
