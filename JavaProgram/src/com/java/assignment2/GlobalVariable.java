144bc0472ec0272136c82e18478e1b6ecd6df880
Pragati <Pragati@LAPTOP-75OS1RCT>
2024-01-25 17:40:05 +0530

    Golbalvariable

---
+++ JavaProgram/src/com/java/assignment2/GlobalVariable.java
@@ -0,0 +1,12 @@
+package com.java.assignment2;
+
+public class GlobalVariable {
+   int y=20;
+	public static void main(String[] args) {
+		GlobalVariable obj=new GlobalVariable();
+		System.out.println(obj.y);
+		
+
+	}
+
+}
