e996b610b79d965ae840a6d0e1aff5279b1979be
Pragati <Pragati@LAPTOP-75OS1RCT>
2024-01-25 17:39:19 +0530

    squareno

---
+++ JavaProgram/src/com/java/Assignment5/SqyareOfNo.java
@@ -0,0 +1,17 @@
+package com.java.Assignment5;
+
+import java.util.Scanner;
+
+public class SqyareOfNo {
+
+	public static void main(String[] args) {
+		System.out.println("Enter a No");
+		Scanner sc=new Scanner(System.in);
+		
+		int num=sc.nextInt();
+		System.out.println("Sq of no= "+num*num);
+		
+
+	}
+
+}
