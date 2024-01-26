6f0fdfad58505e5ec97a1961e65abbbac602a308
Pragati <Pragati@LAPTOP-75OS1RCT>
2024-01-26 11:22:04 +0530

    IfElseLadder

---
+++ JavaProgram/src/com/loop/IfElseLadderNamePrint.java
@@ -0,0 +1,39 @@
+package com.loop;
+
+import java.util.Scanner;
+
+public class IfElseLadderNamePrint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter a value to check multiple:");
+		int i = sc.nextInt();
+		
+    for(i=1;i<=100;i++) {
+		if (i%5) {
+			System.out.println("Pragati");
+		}
+		else if (i%7) {
+			System.out.println("Sonone");
+		}
+		else if {
+		System.out.println("Pragati Sonone");
+	}	
+		}
+		
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
