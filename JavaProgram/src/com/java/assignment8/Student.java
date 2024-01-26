b86f000ef0e1ada1feede88c354bf5e5895c45c5
Pragati <Pragati@LAPTOP-75OS1RCT>
2024-01-26 11:20:10 +0530

    Student

---
+++ JavaProgram/src/com/java/assignment8/Student.java
@@ -0,0 +1,18 @@
+package com.java.assignment8;
+/**Design a student class which contain First name,
+ * last name,city mobile number and country
+ */
+
+public class Student {
+	
+	string firstname;
+	
+	string lastname;
+	
+	string city;
+	
+	long mobileNumber;
+	static String country = "INDIA";
+
+	}
+}
