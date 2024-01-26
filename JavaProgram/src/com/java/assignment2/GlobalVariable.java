4879c56a34237aecca34ca311c1234b68fd1ba76
Pragati <Pragati@LAPTOP-75OS1RCT>
2024-01-26 11:18:06 +0530

    GlobalVariable

--- JavaProgram/src/com/java/assignment2/GlobalVariable.java
+++ JavaProgram/src/com/java/assignment2/GlobalVariable.java
@@ -1,7 +1,9 @@
 package com.java.assignment2;
+//Print Global variable Y 25
+
 
 public class GlobalVariable {
-   int y=20;
+   int y=25;
 	public static void main(String[] args) {
 		GlobalVariable obj=new GlobalVariable();
 		System.out.println(obj.y);
