0d5e88a7b99b94252710d769c240144f74f57e20
Pragati <Pragati@LAPTOP-75OS1RCT>
2024-01-26 11:13:48 +0530

    EvenNo

--- JavaProgram/src/com/java/assignment6/EvenNo.java
+++ JavaProgram/src/com/java/assignment6/EvenNo.java
@@ -4,7 +4,7 @@ public class EvenNo {
 
 	public static void main(String[] args) {
 		
-    for(int i=10;i<=25;i++) {
+    for(int i=50;i<=75;i++) {
     	if(i%2==0) {
     		System.out.println(i);
     	}
