244ea6da082f1020af84d6ced88068fbbfe0d61c
Pragati <Pragati@LAPTOP-75OS1RCT>
2024-01-26 11:28:56 +0530

    PrintNo

---
+++ JavaProgram/src/com/java/assignment6/PrintNo.java
@@ -0,0 +1,13 @@
+package com.java.assignment6;
+
+public class PrintNo {
+
+	public static void main(String[] args) {
+		
+      for (int start=25; start<=50; start++) {
+         System.out.println(start);
+	
+		 
+	}
+}
+}
