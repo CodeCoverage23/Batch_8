8fec1b24cdfb7b63434ab4ca8faaef00708a7448
pratiksha <pratikshanaik3008@gmail.com>
2024-01-27 22:13:04 +0530

    task

---
+++ Batch8/src/com/task/ArrayString.java
@@ -0,0 +1,21 @@
+package com.task;
+
+import java.util.Scanner;
+
+public class ArrayString {
+
+	public static void main(String[] args) {
+//		String []s=new String[5];
+//		Scanner sc=new Scanner(System.in);
+//		for(int i=0;i<5;i++) {
+//			s[i]=sc.next();
+//		}
+//		System.out.println(s.length);
+//		
+		
+		String []s= { "code" ,"coverage","jiya","abhi","naik"};
+		System.out.println(s.length);
+
+	}
+
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Batch_8
-this repository for Batch 8
