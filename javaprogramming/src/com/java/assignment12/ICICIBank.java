1d66fdefb1955e0200712463726ebe115e9f34f1
prade <prade@DESKTOP-9LC5689.lan>
2024-01-29 09:07:12 +0530

    assignment 12

---
+++ javaprogramming/src/com/java/assignment12/ICICIBank.java
@@ -0,0 +1,32 @@
+package com.java.assignment12;
+
+import java.util.Scanner;
+
+public class ICICIBank extends Bank {
+
+	public void getICICIBankDetails() {
+		
+		Scanner sc =new Scanner(System.in);
+		System.out.println("enter the details for icici bank>>");
+		System.out.println("principal amount>>");
+		principleAmount=sc.nextDouble();
+		System.out.println("tenure>>");
+		tenure=sc.nextInt();
+		System.out.println("rate of interest>>");
+		rateOfInterest=sc.nextFloat();
+		double calculateInterest = getCalculateInterest(principleAmount,tenure,rateOfInterest);
+		System.out.println("simple Interest for IcicI Bank>>"+calculateInterest);
+		
+		
+	}
+	 
+	private double getCalculateInterest(double principleAmount, int tenure , float rateOfInterest  ) {
+		double interest=(principleAmount*tenure*rateOfInterest)/100;
+		
+		return  interest;
+	}
+	
+	
+	
+	
+}
---
+++ javaprogramming/src/com/java/assignment12/TestBank.java
@@ -0,0 +1,12 @@
+package com.java.assignment12;
+
+public class TestBank {
+
+	public static void main(String[] args) {
+       
+		ICICIBank bank=new ICICIBank();
+		bank.getICICIBankDetails();
+	
+	}
+
+}
