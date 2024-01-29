12f71fcd26cc7d02cdb8a8e535e3e3ffcf7ef1d0
prade <prade@DESKTOP-9LC5689.lan>
2024-01-29 09:07:34 +0530

    assignment 11

---
+++ javaprogramming/src/com/java/assignment11/College.java
@@ -0,0 +1,13 @@
+package com.java.assignment11;
+//1. I have student class now user restricts to create the object of that class from outside, write the code
+//for it.
+public class College {
+
+	public static void main(String[] args) {
+
+		
+		Students s1=new Students();
+		
+	}
+
+}
---
+++ javaprogramming/src/com/java/assignment11/Students.java
@@ -0,0 +1,13 @@
+package com.java.assignment11;
+
+public class Students {
+
+	private Students(){
+		
+		System.out.println("constructor");
+		
+	}
+	
+	
+	
+}
