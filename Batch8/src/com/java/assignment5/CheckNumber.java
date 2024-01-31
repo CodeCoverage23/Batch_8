b1fb9cb121fb73f174596a41cd28a279a0b5e7f7
DELL <DELL@DESKTOP-6J5K0ME>
2024-01-31 21:28:50 +0530

    Assignment-5

---
+++ Batch8/src/com/java/assignment5/CheckNumber.java
@@ -0,0 +1,28 @@
+package com.java.assignment5;
+
+//1. Write a program to check the number is positive or negative.
+import java.util.Scanner;
+
+public class CheckNumber {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc= new Scanner(System.in);
+		
+		System.out.print("enter a number to check positive or not :");
+		
+		int num = sc.nextInt();
+		
+		if (num>= 0) {
+			
+			System.out.println("the given number is positive");
+		} else {
+			
+			System.out.println("the given number is positive");
+		}
+			
+			
+	}
+ }
+
+
