922bbd01e5894666a643e1e8eedd3033c76855fe
Pragati <Pragati@LAPTOP-75OS1RCT>
2024-02-01 16:58:12 +0530

    Account

---
+++ JavaProgram/src/com/java/assignment13/Account.java
@@ -0,0 +1,12 @@
+package com.java.assignment13;
+//Design the interface which contain below method public void getSavingAccount();
+//public void getUserDeails(String name,float balance);
+
+public interface Account {
+
+	public void getSavingAccount();
+	
+	public void getAccountDetails(String name, float balance);
+	
+	
+}
