7443f5ea36952239f5ddef08e99b6116ca8b0925
dhana <dhana@DHANU>
2024-02-04 08:48:30 +0530

    Assignment 14

---
+++ Java/src/com/assignment14/Table.java
@@ -0,0 +1,15 @@
+package com.assignment14;
+
+public class Table {
+
+	public static void main(String[] args) {
+		for (int i = 2; i <= 20; i++) {
+			for (int j = 1; j <= 10; j++) {
+				int multi = i * j;
+				System.out.println(multi + "\t");
+			}
+			System.out.println();
+		}
+	}
+
+}
