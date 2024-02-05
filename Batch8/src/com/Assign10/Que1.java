c066ba965a5c9e7416a3cba9febacb88a3bc70ca
pratiksha <pratikshanaik3008@gmail.com>
2024-02-05 22:38:37 +0530

    Assignment 10

---
+++ Batch8/src/com/Assign10/Que1.java
@@ -0,0 +1,35 @@
+package com.Assign10;
+
+import java.util.Scanner;
+
+//1. Write Java program to check whether number is prime or not. ( Prime means a number which is
+//divisible by only two numbers: 1 and itself. So, if any number is divisible by any other number, it is not a
+//prime number.)
+public class Que1 {
+	public static boolean isPrime(int n) {
+		int count = 0;
+		for (int i = 1; i <= n; i++) {
+			if (n % i == 0) {
+				count++;
+			}
+		}
+		if (count == 2) {
+			return true;
+		}
+		return false;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number to check whether it is prime or not : ");
+		int n = sc.nextInt();
+		boolean ans = isPrime(n);
+		if (ans == true) {
+			System.out.println(n + " is a Prime number.");
+		} else {
+			System.out.println(n + " is not a Prime number.");
+		}
+
+	}
+
+}
---
+++ Batch8/src/com/Assign10/Que3.java
@@ -0,0 +1,42 @@
+package com.Assign10;
+
+import java.util.Scanner;
+
+//3. Write java program to check whether number is Armstrong or not.(A positive number is called
+//armstrong number if it is equal to the sum of cubes of its digits for example 0, 1, 153, 370, 371, 407 etc.
+//Let's try to understand why 153 is an Armstrong number.
+//153 = (1*1*1)+(5*5*5)+(3*3*3)
+public class Que3 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number to check whether is is armstrong number or not : ");
+		int n = sc.nextInt();
+		if (isArmstrong(n)) {
+			System.out.println(n + " is a armstrong number. ");
+		} else {
+			System.out.println(n + " is not a armstrong number.");
+		}
+
+	}
+
+	public static boolean isArmstrong(int num) {
+		int originalNum, remainder, result = 0, n = 0;
+		originalNum = num;
+		while (originalNum != 0) {
+			originalNum /= 10;
+			n++;
+		}
+		originalNum = num;
+
+		while (originalNum != 0) {
+			remainder = originalNum % 10;
+			result += Math.pow(remainder, n);
+			originalNum /= 10;
+		}
+
+		return result == num;
+
+	}
+
+}
