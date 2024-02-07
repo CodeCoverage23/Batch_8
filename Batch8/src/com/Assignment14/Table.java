63b3752336a74b59e9b5ff361a60dddde49b82a2
LENOVO <LENOVO@LAPTOP-093DM0CO>
2024-02-07 17:31:39 +0530

    Assignment14

---
+++ Batch8/src/com/Assignment14/Table.java
@@ -0,0 +1,24 @@
+package com.Assignment14;
+
+//Write a java program to print the all multiplication table from *2,3,4...to20
+
+public class Table {
+	
+	
+	public static void main(String[] args) {
+		
+		for (int i = 2; i <=20; i++) {
+			
+			
+			for (int j = 1; j <=10; j++) {
+				
+				int multi = i*j;
+				
+				System.out.println(multi+"");
+			}
+			
+			System.out.println();
+		}
+	}
+
+}
