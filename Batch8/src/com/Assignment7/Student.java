45f72f3fde2372c0f544dcd98a176bc81299e3a3
LENOVO <LENOVO@LAPTOP-093DM0CO>
2024-02-07 17:26:54 +0530

    Assignment7

--- Batch8/src/com/Assignment7/Student.java
+++ Batch8/src/com/Assignment7/Student.java
@@ -1,6 +1,26 @@
 package com.Assignment7;
 
+// Design the student class which contain
+//firstName , lastName , city and mobile number and country
+
+
 public class Student {
+	
+	
+	String firstname =  "Nilima";
+	
+	
+	String lastName = "Shejav";
+	
+	String city = "Amravati";
+	
+	long MobileNumber = 00000000000;
+
+
+	public long mbileNumber;
+	
+	static  String country = "India";
+	
 	
 	
 	
