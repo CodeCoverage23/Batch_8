76930194479a834109bf9428209f21411d698c78
pratiksha <pratikshanaik3008@gmail.com>
2024-02-10 23:56:36 +0530

    Assignment 14

---
+++ Batch8/src/com/Assign14/Table.java
@@ -0,0 +1,17 @@
+package com.Assign14;
+
+//1.Write a java program to print the all multiplication table from * 2, 3, 4.... to 20.
+public class Table {
+
+	public static void main(String[] args) {
+		for (int i = 2; i <= 20; i++) {
+			for (int j = 1; j <= 10; j++) {
+				int mul = i * j;
+				System.out.print(mul + "\t");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
