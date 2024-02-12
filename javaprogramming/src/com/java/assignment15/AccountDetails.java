3f13b31f6bbfd3785d6ba7df1195a2de73460257
prade <prade@DESKTOP-9LC5689.lan>
2024-02-12 09:19:32 +0530

    assignment 15 (2nd example)

---
+++ javaprogramming/src/com/java/assignment15/AccountDetails.java
@@ -0,0 +1,49 @@
+package com.java.assignment15;
+
+import java.util.Scanner;
+
+//Design class AccountDetails class in which below methods. 
+//Public void getAccountDetails () 
+//public static int getWithdrawDetails(Account account) 
+
+public class AccountDetails {
+
+	public void getaccountDetails() {
+		
+		Scanner sc= new Scanner(System.in);
+		System.out.println("enter the Id>>");
+		int id=sc.nextInt();
+		System.out.println("enter the AccountNumber>>");
+		String accountNumber=sc.next();
+		System.out.println("enter the balance>>");
+		double balance=sc.nextDouble();
+		
+		Account account=new Account();
+		account.setId(id);
+		account.setAccountNumber(accountNumber);
+		account.setBalance(balance);
+		System.out.println(account);
+		getWithdrawDetails(account);
+		
+	}
+	
+	public static double getWithdrawDetails(Account account) {
+		double currentBalance=0.0;
+		System.out.println("enter the amount for withdrawl>>");
+		Scanner sc=new Scanner(System.in);
+		double withdrawAmount=sc.nextDouble();
+		if(account.getBalance()>withdrawAmount) {
+			currentBalance=(account.getBalance()-withdrawAmount);
+			System.out.println("transaction successfull");
+		System.out.println("your Current Balance is >>"+currentBalance);
+		}
+		else {
+			System.out.println("transaction unsuccessfull");
+			System.out.println("your balance is insufficient");
+		}
+		return currentBalance;
+	}
+	
+	
+	
+}
---
+++ javaprogramming/src/com/java/assignment15/SavingAccount.java
@@ -0,0 +1,24 @@
+package com.java.assignment15;
+//Design another class called as SavingAccount extends Account that contain withdrawAmount. 
+
+public class SavingAccount extends Account {
+	
+	private double withdrawAmount;
+
+	public double getWithdrawAmount() {
+		return withdrawAmount;
+	}
+
+	public void setWithdrawAmount(double withdrawAmount) {
+		this.withdrawAmount = withdrawAmount;
+	}
+
+	@Override
+	public String toString() {
+		return "SavingAccount [withdrawAmount=" + withdrawAmount + "]";
+	}
+	
+	
+	
+
+}
