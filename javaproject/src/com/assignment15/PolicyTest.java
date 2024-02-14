ede3237932a84f1955b122305841883d18fbcf2d
hp <hp@LAPTOP-SABT4769>
2024-02-14 15:17:19 +0530

    Assignment-15.1

---
+++ javaproject/src/com/assignment15/PolicyTest.java
@@ -0,0 +1,15 @@
+package com.assignment15;
+
+public class PolicyTest {
+
+	public static void main(String[] args) {
+		
+		Policy policy=new Policy();
+		
+		Policy policyDetails=policy.getPolicyDetails();
+		
+		System.out.println("Policy Details >>> "  +policyDetails);
+
+	}
+
+}
