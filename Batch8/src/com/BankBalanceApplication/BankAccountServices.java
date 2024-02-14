98a93b7ea0e76b755a986597fd66127d82fa409e
pratiksha <pratikshanaik3008@gmail.com>
2024-02-14 19:41:18 +0530

    code added : passing Scanner class object as a parameter

--- Batch8/src/com/BankBalanceApplication/BankAccountServices.java
+++ Batch8/src/com/BankBalanceApplication/BankAccountServices.java
@@ -8,31 +8,31 @@ public class BankAccountServices {
 	public static double balance = 7000;
 
 	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
 		System.out.println("<<---Welcome to code coverage bank--->>");
-		menu();
+		menu(sc);
 
 	}
 
-	public static void menu() {
-		Scanner sc = new Scanner(System.in);
+	public static void menu(Scanner sc) {
 		System.out.println("menu :");
-		System.out.println("press 1 : transfer money");
+		System.out.println("press 1 : withdraw money");
 		System.out.println("press 2 : deposit money");
 		System.out.println("press 3 : exit from the application");
 		System.out.println("Enter a number of service : ");
 		int n = sc.nextInt();
-		run(n);
+		run(n,sc);
 	}
 
-	public static void run(int n) {
+	public static void run(int n,Scanner sc) {
 		switch (n) {
 		case 1: {
-			transfer();
+			withdraw(sc);
 			break;
 
 		}
 		case 2: {
-			deposit();
+			deposit(sc);
 			break;
 		}
 		case 3: {
@@ -42,33 +42,31 @@ public class BankAccountServices {
 		default: {
 			System.out.println("<<--Please enter valid key..");
 			System.out.println("---------------------------------------");
-			menu();
+			menu(sc);
 		}
 		}
 
 	}
 
-	public static void deposit() {
-		Scanner sc = new Scanner(System.in);
+	public static void deposit(Scanner sc) {
 		System.out.println("Enter the amount to deposit : ");
 		double amount = sc.nextDouble();
 		balance += amount;
 		System.out.println("Amount has been deposited successfully....");
 		System.out.println("Current Balance : " + balance);
 		System.out.println("---------------------------------------");
-		menu();
+		menu(sc);
 	}
 
-	public static void transfer() {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the amount to transfer : ");
+	public static void withdraw(Scanner sc) {
+		System.out.println("Enter the amount to withdraw : ");
 		double amount = sc.nextDouble();
 		if (amount < balance) {
 			balance -= amount;
 			System.out.println("Amount has been transfered successfully...");
 			System.out.println("Current Balance : " + balance);
 			System.out.println("---------------------------------------");
-			menu();
+			menu(sc);
 
 		} else {
 			throw new FundNotFoundException("money to be transfer should be less than account balance ");
