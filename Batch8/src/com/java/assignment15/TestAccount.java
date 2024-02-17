2566b75fe34e3fd76a76c8a6ae8a01f8f099ff1e
HP <HP@DESKTOP-8MV2VQ4>
2024-02-17 21:05:37 +0530

    assignment15

---
+++ Batch8/src/com/java/assignment15/TestAccount.java
@@ -0,0 +1,11 @@
+package com.java.assignment15;
+
+public class TestAccount {
+	public static void main(String[] args) {
+
+		AccountDetails details = new AccountDetails();
+		details.getaccountDetails();
+
+	}
+
+}
