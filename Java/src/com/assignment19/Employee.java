85ce4b251512e785b94922c808e5b4814fe2a3d0
dhana <dhana@DHANU>
2024-02-22 13:00:08 +0530

    Assignment 19

---
+++ Java/src/com/assignment19/Employee.java
@@ -0,0 +1,29 @@
+package com.assignment19;
+
+public class Employee {
+
+	private int id;
+	private String name;
+	private String email;
+	private String mobileNumber;
+	private String address;
+
+	public Employee(int id, String name, String email, String mobileNumber, String address) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.mobileNumber = mobileNumber;
+		this.address = address;
+
+	}
+
+	public void display() {
+
+		System.out.println("Id:" + id);
+		System.out.println("EmpName:" + name);
+		System.out.println("EmpEmail:" + email);
+		System.out.println("EmpMobNo:" + mobileNumber);
+		System.out.println("EmpAddress:" + address);
+	}
+
+}
---
+++ Java/src/com/assignment19/StudentMobileNumberIterator.java
@@ -0,0 +1,34 @@
+package com.assignment19;
+
+//1. Write program to add the five students mobile number 
+//into arraylist and iterate that elements using iterator. 
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class StudentMobileNumberIterator {
+
+	public static void main(String[] args) {
+		ArrayList<String> al = new ArrayList<String>();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Mobile Number: ");
+
+		for (int i = 0; i < 5; i++) {
+			String mobileNumber = sc.next();
+			if (mobileNumber.length() == 10) {
+				al.add(mobileNumber);
+			} else {
+				System.out.println("Enter Valid MobileNumber");
+			}
+
+		}
+		Iterator itr = al.iterator();
+		System.out.println("Iterating the elements using iterator...");
+		while (itr.hasNext()) {
+			System.out.println(itr.next());
+		}
+
+	}
+
+}
