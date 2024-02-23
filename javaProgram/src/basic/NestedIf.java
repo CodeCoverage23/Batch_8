11c144ecd8760f559c67df5a5a1057369af7f4dd
Dell <Dell@DESKTOP-0C6H9US>
2024-02-23 18:12:41 +0530

    nested

---
+++ javaProgram/src/basic/NestedIf.java
@@ -0,0 +1,19 @@
+package basic;
+
+public class NestedIf {
+	public static void main(String[] args) {
+
+		int age = 16;
+		int weight = 55;
+
+		if (age > 18) {
+			System.out.println("Age is greater than 18");
+			if (weight < 50) {
+				System.out.println("You can donate the Blood");
+			}
+
+		}
+
+	}
+
+}
