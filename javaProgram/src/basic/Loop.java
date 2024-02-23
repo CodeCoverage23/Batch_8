c1c9cae168ee1ff25a78e9d760f55fd38348c361
Dell <Dell@DESKTOP-0C6H9US>
2024-02-23 18:13:28 +0530

    loop

---
+++ javaProgram/src/basic/Loop.java
@@ -0,0 +1,23 @@
+package basic;
+
+public class Loop {
+	
+	public static void main(String[] args) {
+
+		
+
+		System.out.println("Code coverage");
+		System.out.println(1 * 2);
+		System.out.println(2 * 2);
+		System.out.println("3");
+		System.out.println("4");
+		System.out.println("5");
+		System.out.println("6");
+		System.out.println("7");
+		System.out.println("8");
+		System.out.println("9");
+		System.out.println("10");
+
+	}
+
+}
