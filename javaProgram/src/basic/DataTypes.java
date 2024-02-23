ba2317fe996ecf1730bafc9e3b4ce8ab4adce887
Dell <Dell@DESKTOP-0C6H9US>
2024-02-23 17:50:42 +0530

    DataTypes

---
+++ javaProgram/src/basic/DataTypes.java
@@ -0,0 +1,27 @@
+package basic;
+
+public class DataTypes {
+
+	byte b = 1;
+	
+	short s = 20;
+	
+	int i = 100;
+	
+	long l = 200;
+	
+	float f = 11.1f;
+	
+	double d = 20.0f;
+	
+	char  c = 'A';
+	
+	boolean b2 = true;
+	
+	static int data;
+	
+	public static void main(String []args)
+	{
+		System.out.println(data);
+	}
+}
