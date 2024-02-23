d62d43f3e5f3e755155b3ca1383c69a494937234
Dell <Dell@DESKTOP-0C6H9US>
2024-02-23 18:14:22 +0530

    ifblock

---
+++ javaProgram/src/basic/IfBlock.java
@@ -0,0 +1,24 @@
+package basic;
+
+public class IfBlock {
+	
+	public static void main(String[] args) {
+
+		int a = 11;
+
+		if (a > 10) {
+			System.out.println("A is greater");
+		}
+
+		System.out.println(a / 2);
+		System.out.println(a % 2);
+
+		if (a % 2 == 0) {
+			System.out.println("This is Even number");
+		} else {
+			System.out.println("This is Odd number");
+		}
+
+	}
+
+}
