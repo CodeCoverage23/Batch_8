2d7ecff115044ad1a218db4b358b58843f539e8a
Dell <Dell@DESKTOP-0C6H9US>
2024-02-23 18:14:52 +0530

    Hello

---
+++ javaProgram/src/basic/Hello.java
@@ -0,0 +1,29 @@
+package basic;
+
+public class Hello {
+	
+	public static void main(String[] args) {
+
+		System.out.println("Hello Java");
+
+		System.out.println(Math.sqrt(16));
+
+
+}
+}
+
+class A {
+	
+}
+
+class B {
+	
+}
+
+class C {
+
+}
+
+class D {
+
+}
