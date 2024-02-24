59c22d99acc548d6ce253402396e7abf25253547
Dell <Dell@DESKTOP-0C6H9US>
2024-02-24 14:13:10 +0530

    student

---
+++ javaProgram/src/com/java/assignment8/Student.java
@@ -0,0 +1,13 @@
+package com.java.assignment8;
+
+public class Student {
+   String firstName;
+	
+	String lastName;
+	
+	String city;
+	
+	long mobileNumber;
+	
+	static String country = " INDIA ";
+}
