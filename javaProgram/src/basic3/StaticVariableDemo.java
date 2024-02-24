3393c7acb736ff4de7c233ca2adf983a9026407d
Dell <Dell@DESKTOP-0C6H9US>
2024-02-24 14:14:46 +0530

    static

---
+++ javaProgram/src/basic3/StaticVariableDemo.java
@@ -0,0 +1,21 @@
+package basic3;
+import com.basic3.Student;
+
+public class StaticVariableDemo {
+	
+	public static void main(String[] args) {
+
+		Student s1 = new Student(101, "Kishor");
+		Student s2 = new Student(102, "Pavan");
+		Student s3 = new Student(103, "Vishal");
+		Student s4 = new Student(104, "Pratik");
+		Student s5 = new Student(105, "Shiv");
+		Student s6 = new Student(106, "Shubham");
+
+		s1.display();
+		s2.display();
+		
+	}
+
+
+}
