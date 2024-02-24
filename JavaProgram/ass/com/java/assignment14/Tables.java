b2c8adcbd5bc1d3214810bc6941546ba97bf1202
dell 5480 <dell 5480@DESKTOP-TDUQJND>
2024-02-24 14:22:35 +0530

    assignment 14

--- JavaProgram/ass/com/java/assignment14/Tables.java
+++ JavaProgram/ass/com/java/assignment14/Tables.java
@@ -1,5 +1,7 @@
 package com.java.assignment14;
 
+//Write a java program to print the all multiplication table from *2,3,4,...20.
+
 public class Tables {
 
 	public static void main(String[] args) {
