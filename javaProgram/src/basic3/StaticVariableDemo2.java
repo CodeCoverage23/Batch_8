ec205064267c78919be189dbffe7b2d977b315b2
Dell <Dell@DESKTOP-0C6H9US>
2024-02-24 14:15:07 +0530

    static

---
+++ javaProgram/src/basic3/StaticVariableDemo2.java
@@ -0,0 +1,29 @@
+package basic3;
+
+public class StaticVariableDemo2 {
+	
+	int id;
+	String name;
+	static String cName = "XYZ";
+
+	public StaticVariableDemo2(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public static void main(String[] args) {
+
+		StaticVariableDemo2 s1 = new StaticVariableDemo2(1010, "Sachin");
+		StaticVariableDemo2 s2 = new StaticVariableDemo2(1011, "Ram");
+
+		s1.print();
+		s2.print();
+
+	}
+
+	public void print() {
+		System.out.println(id + " " + name + " " + cName);
+	}
+
+
+}
