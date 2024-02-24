b9c69835993f249471f6161c45fe3b41e226e8e4
Dell <Dell@DESKTOP-0C6H9US>
2024-02-24 14:20:29 +0530

    multiplication

---
+++ javaProgram/src/com/java/assignment14/MultiplicationTable.java
@@ -0,0 +1,26 @@
+package com.java.assignment14;
+
+public class MultiplicationTable {
+	
+public static void main(String[] args) {
+	
+		
+		System.out.print("Print Multipliction Table : ");
+
+		
+		
+		for (int i= 1; i<=20; i++) {
+			
+			for (int j=1; j<=10; j++) {
+				
+				int multi = i*j;
+				
+				System.out.println(multi);
+				
+			}
+			System.out.println(" ------ ");
+			
+			}
+		}
+
+}
