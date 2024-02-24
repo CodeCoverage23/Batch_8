725ca7b2ee7a5fb099a7a7923561a84383901dca
Dell <Dell@DESKTOP-0C6H9US>
2024-02-24 14:21:47 +0530

    missing

---
+++ javaProgram/src/com/java/assignment16/MissingNumberInArray.java
@@ -0,0 +1,32 @@
+package com.java.assignment16;
+
+
+
+	public class MissingNumberInArray {
+
+		public static void main(String[] args) {
+
+			int a[] = { 1, 2, 3, 5, 6, 7, 9, 10 };
+
+			System.out.println(" Missing Number : ");
+			
+			for (int num = 1; num <= a.length; num++) {
+
+				int count = 0;
+
+				for (int j = 0; j < a.length; j++) {
+					
+					if (num == a[j]) {
+						count++;
+					}
+				}
+
+				if (count == 0) {
+					System.out.println(num);
+				}
+			}
+
+		}
+
+	}
+
