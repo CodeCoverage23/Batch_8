9ce38682c87e5cefedcc9f0e52aa09be899f71d7
Dell <Dell@DESKTOP-0C6H9US>
2024-02-24 14:26:16 +0530

    employee

---
+++ javaProgram/src/com/java/assignment19/Employee.java
@@ -0,0 +1,31 @@
+
+package com.java.assignment19;
+
+public class Employee {
+
+	private int id;
+	private String name;
+	private String mobileNumber;
+	private String email;
+	private String address;
+
+	public Employee(int id, String name, String mobileNumber, String email, String address) {
+
+		this.id = id;
+		this.name = name;
+		this.mobileNumber = mobileNumber;
+		this.email = email;
+		this.address = address;
+
+	}
+
+	public void display() {
+
+		System.out.println("id : " + id);
+		System.out.println("Name : " + name);
+		System.out.println("MobileNumber : " + mobileNumber);
+		System.out.println("email : " + email);
+		System.out.println("Address: " + address);
+	}
+
+}
