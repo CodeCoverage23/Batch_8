14acb8419eed06c71e66d88b757a1bdf9dd3c918
Dell <Dell@DESKTOP-0C6H9US>
2024-02-24 14:19:40 +0530

    parameter

---
+++ javaProgram/src/basic3/ParameterizedConstructorDemo.java
@@ -0,0 +1,23 @@
+package basic3;
+
+public class ParameterizedConstructorDemo {
+	
+	int a;
+
+	String b;
+
+	ParameterizedConstructorDemo(int data, String s) {
+		a = data;
+		b = s;
+
+		System.out.println(a + b);
+	}
+
+	public static void main(String[] args) {
+
+		ParameterizedConstructorDemo p = new ParameterizedConstructorDemo(10, "Code");
+
+	}
+}
+
+
