7a22e5586771b6f9a16dda454b5d5bfdaad2ef07
dhana <dhana@DHANU>
2024-03-08 23:54:17 +0530

    Assignment 23.1

---
+++ Java/src/com/assignment23/Employee.java
@@ -0,0 +1,30 @@
+package com.assignment23;
+
+//Suppose I have Employee class that containing Id, name and city 
+//now I want to sort the data based on comparable interface by using name.
+
+public class Employee implements Comparable<Employee> {
+
+	int id;
+	String name;
+	String city;
+
+	public Employee(int id, String name, String city) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.city = city;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", city=" + city + "]";
+	}
+
+	@Override
+	public int compareTo(Employee o) {
+
+		return this.name.compareTo(o.name);
+	}
+
+}
