8f07e38b12fbb43800040c2fd4f59b5f31b24ff6
user <user@DESKTOP-FD8J3KF>
2024-03-08 18:26:50 -0800

    Assignment13

---
+++ Batch8/src/com/assignment13/Account.java
@@ -0,0 +1,12 @@
+package com.assignment13;
+
+public interface Account {
+	
+//1. Design the interface which contain below method public void getSavingAccount();
+//public void getUserDetails(String name,float blance);
+
+public void getSavingAccount();
+
+public void getAccountDetails(String name,float balance);
+
+}
