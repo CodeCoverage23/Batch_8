232d205419a8dec054751e942ce9ffa640d17893
user <user@DESKTOP-FD8J3KF>
2024-03-13 04:53:19 -0700

    assignment15Q1

---
+++ Batch8/src/com/assignment15/Policy.java
@@ -0,0 +1,88 @@
+package com.assignment15;
+
+import java.util.Scanner;
+
+public class Policy {
+	private int id ;
+	private String policyName;
+	private String policyType;
+	private int premiumAmount;
+	
+	
+	
+	public int getId() {
+		return id;
+	}
+
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+
+	public String getPolicyName() {
+		return policyName;
+	}
+
+
+
+	public void setPolicyName(String policyName) {
+		this.policyName = policyName;
+	}
+
+
+
+	public String getPolicyType() {
+		return policyType;
+	}
+
+
+
+	public void setPolicyType(String policyType) {
+		this.policyType = policyType;
+	}
+
+
+
+	public int getPremiumAmount() {
+		return premiumAmount;
+	}
+
+
+
+	public void setPremiumAmount(int premiumAmount) {
+		this.premiumAmount = premiumAmount;
+	}
+
+	
+	public Policy getPolicyDetails() {
+		
+		Scanner sc =new Scanner(System.in);
+		System.out.println("Enter Policy Id >>");
+		id=sc.nextInt();
+		System.out.println("Enter the Policy name >>");
+		policyName=sc.next();
+		System.out.println("Enter the Policytype >>");
+		policyType=sc.next();
+		System.out.println("Enter the primiumAmount >>");
+		premiumAmount=sc.nextInt();
+		
+	     Policy policy=new Policy();
+	     policy.setId(id);
+	     policy.setPolicyName(policyName);
+	     policy.setPolicyType(policyType);
+	     policy.setPremiumAmount(premiumAmount);
+		
+		return policy;
+	}	
+	@Override
+	public String toString() {
+		return "Policy [id=" + id + ", policyName=" + policyName + ", policyType=" + policyType + ", premiumAmount="
+				+ premiumAmount + "]";
+	}
+
+}
+
+
---
+++ Batch8/src/com/assignment15/PolicyTest.java
@@ -0,0 +1,12 @@
+package com.assignment15;
+
+public class PolicyTest {
+	
+	public static void main(String[] args) {
+		Policy policy=new Policy();
+		
+		Policy policyDetails = policy.getPolicyDetails();
+		 System.out.println("policy data is >> "+policyDetails);
+
+
+}}
